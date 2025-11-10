7bd3809d8e3f1f87e5780113f013b02f7ca7d01f
Pablo José Pellegrino Cintra <pablopellegrinocintra@hotmail.com>
2025-11-10 19:42:25 -0300

    add função InicializarJogo
    necessita correção

--- src/main.c
+++ src/main.c
@@ -6,3 +6,17 @@ typedef enum {
     TELA_JOGO,
     TELA_GAMEOVER
 } TelaJogo;
+
+static void InicializarJogo(void) {
+    InitWindow(SCREEN_W_DEFAULT, SCREEN_H_DEFAULT, "Cosmic Blaster");
+    SetTargetFPS(60);
+
+    melhorPontuacao = LoadBestScore("score.txt");
+
+    balas = NULL;
+    asteroides = NULL;
+    nave = CriarNave();
+    campoEstrelas = CriarCampoEstrelas(100, 50, SCREEN_W_DEFAULT, SCREEN_H_DEFAULT);
+
+    telaAtual = TELA_TITULO;
+}
