317b3574e9ff8fa1a39100ba1848d083de26e0f3
Yan Ribeiro Nunes <yrn@cesar.school>
2025-11-18 19:31:50 -0300

    removendo textura na cor da nave e modificando a funcao AtualizarNave

--- src/game.c
+++ src/game.c
@@ -155,8 +155,7 @@ void AtualizarNave(Nave* nave, float dt) {
     if (nave == NULL) {
         return;
     }
-    Vector2 direcao = { 0, 0 };
-
+    Vector2 direcao = {0, 0};
     if (IsKeyDown(KEY_A)) direcao.x = -1;
     if (IsKeyDown(KEY_D)) direcao.x =  1;
     if (IsKeyDown(KEY_W)) direcao.y = -1;
@@ -169,17 +168,14 @@ void AtualizarNave(Nave* nave, float dt) {
 
         nave->posicao.x += direcao.x * nave->velocidade * dt;
         nave->posicao.y += direcao.y * nave->velocidade * dt;
+
         nave->rotacao = atan2f(direcao.y, direcao.x) * (180 / PI) + 90;
     }
     if (IsKeyPressed(KEY_TAB)) {
-        if (nave->textura == &texturaNaveAzul){
-            nave->textura = &texturaNaveVermelha;
-        }
-        else{
-            nave->textura = &texturaNaveAzul;
-        }
+        nave->textura = &texturaNaveAzul;
     }
 }
+
 static void DesenharCentralizado(Texture2D textura, Vector2 pos, float rot, float escala) {
     Rectangle src = {
         0,0,(float)textura.width,(float)textura.height
